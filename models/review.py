cf99044e1ed4550f4ae0f449e48450a9396bd66e
Shajoshed20 <ugobelle@gmail.com>
2023-09-19 13:31:52 +0100

    modified reviews

--- models/review.py
+++ models/review.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project """
+""" Define Review class module """
+from models.base_model import Base
+from os import getenv
 from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class Review(BaseModel):
-    """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+class Review(BaseModel, ):
+    """ Representation for Review class """
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        text = ""
+        place_id = ""
+        user_id = ""
+    else:
+        __tablename__ = "reviews"
+        text = Column(String(1024), nullable=False)
+        place_id = Column(String(60), ForeignKey("places.id"),
+                          nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"),
+                         nullable=False)
