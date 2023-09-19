adbb50e2ac402ac2bf2baf2946d9385a7462d831
Iyioluwa Awe <86264371+iyiolakeni@users.noreply.github.com>
2023-09-19 09:19:17 +0100

    Update test_user.py

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -4,31 +4,41 @@ from tests.test_models.test_base_model import TestBaseModel
 from models.user import User
 
 
-class test_User(TestBaseModel):
-    """ """
+import unittest
+from models.user import User
+from models.base_model import BaseModel
+from models.base import Base
+from sqlalchemy import Column, String
 
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = "User"
-        self.value = User
+class TestUser(unittest.TestCase):
 
-    def test_first_name(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.first_name), str)
+    def test_attributes(self):
+        """Check if User class inherits from BaseModel and Base"""
+        self.assertTrue(issubclass(User, BaseModel))
+        self.assertTrue(issubclass(User, Base))
 
-    def test_last_name(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.last_name), str)
+        # Create an instance of User
+        user = User()
 
-    def test_email(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.email), str)
+        """ Check if class attributes exist"""
+        self.assertTrue(hasattr(User, '__tablename__'))
+        self.assertTrue(hasattr(User, 'email'))
+        self.assertTrue(hasattr(User, 'password'))
+        self.assertTrue(hasattr(User, 'first_name'))
+        self.assertTrue(hasattr(User, 'last_name'))
 
-    def test_password(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.password), str)
+        """Check the data types of class attributes"""
+        self.assertIsInstance(User.__tablename__, str)
+        self.assertIsInstance(User.email.property.columns[0].type, String)
+        self.assertIsInstance(User.password.property.columns[0].type, String)
+        self.assertIsInstance(User.first_name.property.columns[0].type, String)
+        self.assertIsInstance(User.last_name.property.columns[0].type, String)
+
+        """ Check the nullability of attributes"""
+        self.assertFalse(User.email.property.columns[0].nullable)
+        self.assertFalse(User.password.property.columns[0].nullable)
+        self.assertTrue(User.first_name.property.columns[0].nullable)
+        self.assertTrue(User.last_name.property.columns[0].nullable)
+
+if __name__ == '__main__':
+    unittest.main()
