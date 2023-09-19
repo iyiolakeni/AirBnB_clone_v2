26467827abc6bc29b55c9a1e0de47391e1f67e32
Iyioluwa Awe <86264371+iyiolakeni@users.noreply.github.com>
2023-09-19 02:54:39 +0100

    Update state.py to create relationship with cities

--- models/state.py
+++ models/state.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
-
+from models.base_model import Base
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = 'states'  # Table name is 'states'
+    name = Column(String(128), nullable=False)  # Column for state name
+
+    # Define the relationship with City for DBStorage
+    if os.environ.get('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City", backref="state", cascade="all, delete-orphan")
+    else:
+        @property
+        def cities(self):
+            """Getter attribute to return a list of City instances with state_id equal to State.id"""
+            from models import storage
+            city_list = []
+            for city in storage.all("City").values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
