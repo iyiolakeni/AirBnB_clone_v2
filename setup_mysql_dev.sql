5fdc4a029389618bc7515152d35b6798d6592b9c
Iyioluwa Awe <86264371+iyiolakeni@users.noreply.github.com>
2023-09-19 01:47:40 +0100

    Update setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -9,12 +9,10 @@ CREATE USER
 -- Grant Priviledges to the user 'hbnb_dev'
 GRANT ALL PRIVILEGES
    ON `hbnb_dev_db`.*
-   TO 'hbnb_dev'@'localhost'
-   IDENTIFIED BY 'hbnb_dev_pwd';
+   TO 'hbnb_dev'@'localhost';
 GRANT SELECT
    ON `performance_schema`.*
-   TO 'hbnb_dev'@'localhost'
-   IDENTIFIED BY 'hbnb_dev_pwd';
+   TO 'hbnb_dev'@'localhost';
 
 -- Reload priviledges to apply the changes
 FLUSH PRIVILEGES;
