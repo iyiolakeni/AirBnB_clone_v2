a01ea967de7b603bbeadc77acacf67e56ccf7b9a
iyiolakeni <iyiolakeni@gmail.com>
2023-09-19 00:42:54 +0000

    Styling

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,19 +2,18 @@
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
 -- Create the user 'hbnb_dev' if it doesn't exist
-CREATE USER
-    IF NOT EXISTS 'hbnb_dev'@'localhost'
-    IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF
+	NOT EXISTS 'hbnb_dev'@'localhost'
+	IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- Grant Priviledges to the user 'hbnb_dev'
-GRANT ALL PRIVILEGES
-   ON `hbnb_dev_db`.*
-   TO 'hbnb_dev'@'localhost'
-   IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.*
+	TO 'hbnb_dev'@'localhost'
+	IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT SELECT
-   ON `performance_schema`.*
-   TO 'hbnb_dev'@'localhost'
-   IDENTIFIED BY 'hbnb_dev_pwd';
+	ON `performance_schema`.*
+	TO 'hbnb_dev'@'localhost'
+	IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- Reload priviledges to apply the changes
 FLUSH PRIVILEGES;
