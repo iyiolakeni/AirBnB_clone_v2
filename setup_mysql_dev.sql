fb7873709fbce16d49274717e8c92869e96f2c48
Iyioluwa Awe <86264371+iyiolakeni@users.noreply.github.com>
2023-09-19 01:30:08 +0100

    Add files via upload

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,20 @@
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create the user 'hbnb_dev' if it doesn't exist
+CREATE USER
+    IF NOT EXISTS 'hbnb_dev'@'localhost'
+    IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant Priviledges to the user 'hbnb_dev'
+GRANT ALL PRIVILEGES
+   ON `hbnb_dev_db`.*
+   TO 'hbnb_dev'@'localhost'
+   IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT SELECT
+   ON `performance_schema`.*
+   TO 'hbnb_dev'@'localhost'
+   IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Reload priviledges to apply the changes
+FLUSH PRIVILEGES;
