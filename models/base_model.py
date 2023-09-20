157a2c7e8fd6005b26af662455c6132c211c5238
Iyioluwa Awe <86264371+iyiolakeni@users.noreply.github.com>
2023-09-20 08:40:40 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -10,7 +10,7 @@ Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
-    id = Column(String(60), primary_key=True, nullable=False)
+    id = ""
     created_at = datetime.utcnow()
     updated_at = datetime.utcnow()
     
