4f45008f32b4a04a9e3d7e196f167288f632e323
Iyioluwa Awe <86264371+iyiolakeni@users.noreply.github.com>
2023-09-20 09:29:18 +0100

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -2,7 +2,7 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel
 from models.base_model import Base
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.declarative import Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 import os
