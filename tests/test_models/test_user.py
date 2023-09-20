ca6c7e90f117a28415a8c459a5fec930fb367e4f
Iyioluwa Awe <86264371+iyiolakeni@users.noreply.github.com>
2023-09-20 08:28:33 +0100

    Removed double imports

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 """ """
-from tests.test_models.test_base_model import TestBaseModel
-from models.user import User
-
-
 import unittest
+from tests.test_models.test_base_model import TestBaseModel
 from models.user import User
 from models.base_model import BaseModel
 from models.base import Base
