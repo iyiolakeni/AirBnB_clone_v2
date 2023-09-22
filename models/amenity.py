05673d51f82bf10274379a1fe2fc2e7cfef57e7e
Iyioluwa Awe <86264371+iyiolakeni@users.noreply.github.com>
2023-09-22 20:09:18 +0100

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,22 +1,30 @@
 #!/usr/bin/python3
-""" Defines the Amenity class  """
-from models.base_model import Base
-from models.base_model import BaseModel
+"""Defines the Amenity class."""
 from sqlalchemy import Column
 from sqlalchemy import String
-from sqlalchemy.orm import relationship
-import os
-import models
-
+from sqlalchemy import Table
+from sqlalchemy import ForeignKey
+from models.base_model import BaseModel
+from models.base_model import Base
 
 class Amenity(BaseModel, Base):
-    """Represents of Amenity """
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'amenities'
-        name = Column(String(128), nullable=False)
-    else:
-        name = ""
+    """Represents an Amenity for a MySQL database.
 
-    def __init__(self, *args, **kwargs):
-        """initializes Amenity"""
-        super().__init__(*args, **kwargs)
+    Inherits from SQLAlchemy Base and links to the MySQL table amenities.
+
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store Amenities.
+        name (sqlalchemy String): The amenity name.
+        place_amenities (sqlalchemy relationship): Place-Amenity relationship.
+    """
+    __tablename__ = "amenities"
+    
+    name = Column(String(128), nullable=False)
+    
+    place_amenities = Table(
+        "place_amenity", Base.metadata,
+        Column("place_id", String(60), ForeignKey("places.id"),
+               primary_key=True, nullable=False),
+        Column("amenity_id", String(60), ForeignKey("amenities.id"),
+               primary_key=True, nullable=False)
+    )
