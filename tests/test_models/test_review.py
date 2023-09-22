d97ce5fbebf4d0f055f68a6abb2f69c2ba36b608
Iyioluwa Awe <86264371+iyiolakeni@users.noreply.github.com>
2023-09-22 22:08:58 +0100

    Update test_review.py

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,36 +1,87 @@
 #!/usr/bin/python3
-""" Unittests for testing the Review class"""
-from os import getenv
-from tests.test_models.test_base_model import TestBaseModel
+"""Defines unittests for models/review.py."""
+import os
+import pep8
+import unittest
+from datetime import datetime
+from models.base_model import BaseModel
 from models.review import Review
+from models.engine.file_storage import FileStorage
+from sqlalchemy.exc import OperationalError
 
 
-class test_review(TestBaseModel):
-    """ """
+class TestReview(unittest.TestCase):
+    """Unittests for testing the Review class."""
 
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = "Review"
-        self.value = Review
+    @classmethod
+    def setUpClass(cls):
+        """Review testing setup."""
+        cls.filestorage = FileStorage()
+        cls.review = Review(text="stellar")
 
-    def test_text(self):
-        """ test review text"""
-        new = self.value()
-        self.assertEqual(type(new.text), str if
-                         getenv('HBNB_TYPE_STORAGE') != 'db'
-                         else type(None))
+    @classmethod
+    def tearDownClass(cls):
+        """Review testing teardown."""
+        del cls.review
+        del cls.filestorage
 
-    def test_place_id(self):
-        """ test review place_id"""
-        new = self.value()
-        self.assertEqual(type(new.place_id), str if
-                         getenv('HBNB_TYPE_STORAGE') != 'db'
-                         else type(None))
+    def test_pep8(self):
+        """Test pep8 styling."""
+        style = pep8.StyleGuide(quiet=True)
+        p = style.check_files(["models/review.py"])
+        self.assertEqual(p.total_errors, 0, "Fix pep8")
 
-    def test_user_id(self):
-        """ test review user_id"""
-        new = self.value()
-        self.assertEqual(type(new.user_id), str if
-                         getenv('HBNB_TYPE_STORAGE') != 'db'
-                         else type(None))
+    def test_docstrings(self):
+        """Check for docstrings."""
+        self.assertIsNotNone(Review.__doc__)
+
+    def test_attributes(self):
+        """Check for attributes."""
+        self.assertIsInstance(self.review.id, str)
+        self.assertIsInstance(self.review.created_at, datetime)
+        self.assertIsInstance(self.review.updated_at, datetime)
+        self.assertTrue(hasattr(self.review, "__tablename__"))
+        self.assertTrue(hasattr(self.review, "text"))
+
+    def test_is_subclass(self):
+        """Check that Review is a subclass of BaseModel."""
+        self.assertTrue(issubclass(Review, BaseModel))
+
+    def test_init(self):
+        """Test initialization."""
+        self.assertIsInstance(self.review, Review)
+
+    def test_str(self):
+        """Test __str__ representation."""
+        s = self.review.__str__()
+        self.assertIn("[Review] ({})".format(self.review.id), s)
+        self.assertIn("'id': '{}'".format(self.review.id), s)
+        self.assertIn("'created_at': {}".format(
+            repr(self.review.created_at)), s)
+        self.assertIn("'updated_at': {}".format(
+            repr(self.review.updated_at)), s)
+        self.assertIn("'text': '{}'".format(self.review.text), s)
+
+    def test_save_filestorage(self):
+        """Test save method with FileStorage."""
+        old = self.review.updated_at
+        self.review.save()
+        self.assertLess(old, self.review.updated_at)
+        with open("file.json", "r") as f:
+            self.assertIn("Review." + self.review.id, f.read())
+
+    def test_to_dict(self):
+        """Test to_dict method."""
+        review_dict = self.review.to_dict()
+        self.assertEqual(dict, type(review_dict))
+        self.assertEqual(self.review.id, review_dict["id"])
+        self.assertEqual("Review", review_dict["__class__"])
+        self.assertEqual(self.review.created_at.isoformat(),
+                         review_dict["created_at"])
+        self.assertEqual(self.review.updated_at.isoformat(),
+                         review_dict["updated_at"])
+        self.assertEqual(self.review.text, review_dict["text"])
+
+
+if __name__ == "__main__":
+    unittest.main()
