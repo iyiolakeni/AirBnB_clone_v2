8cfef634749ce0eeabe0ab7fa4fcc71a24817cd5
Iyioluwa Awe <86264371+iyiolakeni@users.noreply.github.com>
2023-09-22 22:06:40 +0100

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,23 +1,13 @@
 #!/usr/bin/python3
-""" Define Review class module """
-from models.base_model import Base
-from os import getenv
-from models.base_model import BaseModel
-from sqlalchemy import Column, String
-from sqlalchemy import ForeignKey
+"""Defines the Review class."""
+from models.base_model import Base, BaseModel
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
-class Review(BaseModel, ):
-    """ Representation for Review class """
-    if getenv('HBNB_TYPE_STORAGE') != 'db':
-        text = ""
-        place_id = ""
-        user_id = ""
-    else:
-        __tablename__ = "reviews"
-        text = Column(String(1024), nullable=False)
-        place_id = Column(String(60), ForeignKey("places.id"),
-                          nullable=False)
-        user_id = Column(String(60), ForeignKey("users.id"),
-                         nullable=False)
+class Review(BaseModel, Base):
+    """Representation for Review class."""
+    __tablename__ = "reviews"
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
