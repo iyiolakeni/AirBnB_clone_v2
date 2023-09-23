12c9d9a9768867151cbf200274b2da6f486c765e
Iyioluwa Awe <86264371+iyiolakeni@users.noreply.github.com>
2023-09-23 22:53:06 +0100

    Update place.py

--- models/place.py
+++ models/place.py
@@ -2,18 +2,26 @@
 """Defines the Place class."""
 import models
 from os import getenv
-from models.base_model import Base, BaseModel
+from models.base_model import Base
+from models.base_model import BaseModel
 from models.amenity import Amenity
 from models.review import Review
-from sqlalchemy import Column, Float, ForeignKey, Integer, String, Table
+from sqlalchemy import Column
+from sqlalchemy import Float
+from sqlalchemy import ForeignKey
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy import Table
 from sqlalchemy.orm import relationship
 
-association_table = Table(
-    "place_amenity", Base.metadata,
-    Column("place_id", String(60), ForeignKey("places.id"),
-           primary_key=True, nullable=False),
-    Column("amenity_id", String(60), ForeignKey("amenities.id"),
-           primary_key=True, nullable=False))
+
+association_table = Table("place_amenity", Base.metadata,
+                          Column("place_id", String(60),
+                                 ForeignKey("places.id"),
+                                 primary_key=True, nullable=False),
+                          Column("amenity_id", String(60),
+                                 ForeignKey("amenities.id"),
+                                 primary_key=True, nullable=False))
 
 
 class Place(BaseModel, Base):
