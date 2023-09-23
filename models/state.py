da4b076ac0f11851adf10845a272f5141ed9a814
Iyioluwa Awe <86264371+iyiolakeni@users.noreply.github.com>
2023-09-23 08:16:09 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
-"""Defines the State class."""
-from os import getenv
-from models.base_model import BaseModel, Base
-from models.city import City
+""" State Module for HBNB project """
+from models.base_model import BaseModel
+from models.base_model import Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-import models
+from os import getenv
 
 class State(BaseModel, Base):
-    """Represents a state for a MySQL database"""
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
+    """ State class """
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state", cascade="all, delete-orphan")
+    else:
+        name = ""
 
-    if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
-            """Get a list of all related City objects."""
+            """Getter attribute that returns the list of City instances with state_id
+            equals to the current State.id"""
             city_list = []
-            for city in list(models.storage.all(City).values()):
+            for city in models.storage.all(City).values():
                 if city.state_id == self.id:
                     city_list.append(city)
             return city_list
-
-    else:
-        cities = relationship("City", backref="state", cascade="delete")
