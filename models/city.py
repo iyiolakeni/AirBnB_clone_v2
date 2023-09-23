7cdf8c2768b1dcb60bb86b221aef49ee3861dbea
Iyioluwa Awe <86264371+iyiolakeni@users.noreply.github.com>
2023-09-23 08:14:05 +0100

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3
-"""Defines the City class."""
-from sqlalchemy import Column
-from sqlalchemy import ForeignKey
-from sqlalchemy import String
-from sqlalchemy.orm import relationship
+""" City Module for HBNB project """
 from models.base_model import BaseModel
-from models.base_model import Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
 
 class City(BaseModel, Base):
-    """Represents a City for a MySQL database.
-
-    Inherits from SQLAlchemy Base and links to the MySQL table cities.
-
-    Attributes:
-        __tablename__ (str): The name of the MySQL table to store Cities.
-        name (sqlalchemy String): The city name.
-        state_id (sqlalchemy String): The state id.
-        places (sqlalchemy relationship): Relationship to Place class.
-    """
-    __tablename__ = "cities"
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-    places = relationship("Place", backref="cities", cascade="delete")
+    """ The city class, contains state ID and name """
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'cities'
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        places = relationship("Place", backref="cities")
+    else:
+        state_id = ""
+        name = ""
