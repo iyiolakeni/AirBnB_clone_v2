05c636cfe1eff8f9a824e09209a0c0a324feca4f
Iyioluwa Awe <86264371+iyiolakeni@users.noreply.github.com>
2023-09-23 07:40:51 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -8,18 +8,10 @@ from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """
-    Represents a state for a MySQL database.
-
-    Inherits from SQLAlchemy Base and links to the MySQL table states.
-
-    Attributes:
-        __tablename__ (str): The name of the MySQL table to store States.
-        name (sqlalchemy String): The name of the State.
-        cities (sqlalchemy relationship): The State-City relationship.
-    """
+    """Represents a state for a MySQL database"""
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="delete")
     
     if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
@@ -30,5 +22,3 @@ class State(BaseModel, Base):
                 if city.state_id == self.id:
                     city_list.append(city)
             return city_list
-
-    cities = relationship("City", backref="state", cascade="delete")
