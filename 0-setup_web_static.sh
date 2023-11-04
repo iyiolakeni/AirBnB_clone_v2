df8e43d2b4ad480b6b68b3bef7da5d5c265fe16a
Iyioluwa Awe <86264371+iyiolakeni@users.noreply.github.com>
2023-11-04 08:58:35 +0100

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,6 +2,7 @@
 # Bash script that sets up web servers for the deployment of web_static
 sudo apt-get update
 sudo apt-get -y install nginx
+sudo apt install ufw
 sudo ufw allow 'Nginx HTTP'
 
 sudo mkdir -p /data/
