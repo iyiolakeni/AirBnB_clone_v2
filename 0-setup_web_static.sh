d570124e6d8251902a6e7564c954e2ddd9e81b2c
Iyioluwa Awe <86264371+iyiolakeni@users.noreply.github.com>
2023-11-04 12:50:01 +0100

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,25 +1,16 @@
 #!/usr/bin/env bash
 # Bash script that sets up web servers for the deployment of web_static
-
-# Update and install Nginx
 sudo apt-get update
 sudo apt-get -y install nginx
-
-# Allow incoming HTTP traffic (port 80) with iptables
-sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
-
-# Save the iptables rules to persist across reboots
-sudo iptables-save > /etc/iptables.rules
-
-# Install and enable the Uncomplicated Firewall (UFW)
-sudo apt-get -y install ufw
 sudo ufw allow 'Nginx HTTP'
-sudo ufw enable
 
-# Create directory structure
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
-echo "<html>
+sudo echo "<html>
   <head>
   </head>
   <body>
@@ -27,14 +18,10 @@ echo "<html>
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
-# Create symbolic link
 sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
-# Change ownership
 sudo chown -R ubuntu:ubuntu /data/
 
-# Configure Nginx to serve the static content
 sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
-# Restart Nginx
 sudo service nginx restart
