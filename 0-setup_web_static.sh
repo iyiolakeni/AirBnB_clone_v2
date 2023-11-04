ab7c1ea0941efb4d2c8d8d3497c68debaf6a1ffe
Iyioluwa Awe <86264371+iyiolakeni@users.noreply.github.com>
2023-11-04 08:56:20 +0100

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,52 +1,27 @@
 #!/usr/bin/env bash
-# Install Nginx if not already installed
+# Bash script that sets up web servers for the deployment of web_static
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 
-sudo useradd ubuntu
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
-if ! dpkg -l | grep -q nginx; then
-    sudo apt-get update
-    sudo apt-get -y install nginx
-fi
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
-# Create necessary directories and files
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
+sudo chown -R ubuntu:ubuntu /data/
 
-# Create symbolic link
-if [ -L /data/web_static/current ]; then
-    sudo rm /data/web_static/current
-fi
-sudo ln -sf /data/web_static/releases/test /data/web_static/current
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
-# Give ownership to the ubuntu user and group recursively
-sudo chown -R ubuntu:ubuntu /data
-
-# Update Nginx configuration
-config_file="/etc/nginx/sites-available/default"
-config_content=$(cat <<EOF
-server {
-    listen 80;
-    server_name _;
-
-    location /hbnb_static {
-        alias /data/web_static/current;
-    }
-
-    location / {
-        add_header X-Served-By $HOSTNAME;
-        proxy_set_header Host $HOSTNAME;
-        proxy_pass http://127.0.0.1:5000;
-    }
-
-    error_page 404 /404.html;
-    location = /404.html {
-        root /usr/share/nginx/html;
-        internal;
-    }
-}
-EOF
-)
-echo "$config_content" | sudo tee "$config_file"
-
-# Restart Nginx
 sudo service nginx restart
